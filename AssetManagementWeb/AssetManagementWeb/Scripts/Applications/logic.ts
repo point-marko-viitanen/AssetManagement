418e751da3cc2a6d6913e83e08e403b84083fb8b
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2016-10-23 20:43:06 +0300

    Lisätty typescript tuki projektiin, aloitettu jsontestisivun toteutus.

---
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Applications/logic.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+
+
+
+
+function initAssetAssignment() {
+    $("#AssignAssetButton").click(function () {
+        alert("Toimii!");
+    });
+}
