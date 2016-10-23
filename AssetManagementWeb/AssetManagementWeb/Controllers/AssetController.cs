a14b7aadbba1c53afd9bd2783624bf2ed7c158a4
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2016-10-23 18:22:48 +0300

    Lisätty toiminto nappi

--- AssetManagementWeb/AssetManagementWeb/Controllers/AssetController.cs
+++ AssetManagementWeb/AssetManagementWeb/Controllers/AssetController.cs
@@ -18,6 +18,13 @@ namespace AssetManagementWeb.Controllers
             return View();
         }
 
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+
+
         // GET: Asset/Details/5
         public ActionResult Details(int id)
         {
